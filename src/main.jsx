f4e7e07988670788d16e3387e33ebe93c329adbd
Poorna <purnachandra1619@gmail.com>
2026-07-08 15:30:03 +0530

    refactor: update hardcoded and env Google Login Client ID values with new credentials

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 import "./theme.css";
 import "./netflix-theme.css";
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "244988304520-ndhnfmpejnlt73mejeo62nbussveh4vq.apps.googleusercontent.com";
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "781290030877-jck1t8r7ue35skj8llrdm0tg1u88cj8p.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
